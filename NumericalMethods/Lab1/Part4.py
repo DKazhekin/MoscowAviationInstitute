11d2db991bdfa4bf204ebf920070e347376a9f2e
deniskazhekin <deniskazhekin@yandex.ru>
2024-03-26 15:47:51 +0300

    Part 4 is done

---
+++ NumericalMethods/Lab1/Part4.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+
+class Eigen:
+
+    def __init__(self, A: np.ndarray, eps: float):
+        self.A = A
+        self.eps = eps
+
+    @property
+    def A(self):
+        return self._A
+
+    @A.setter
+    def A(self, matrix: np.ndarray):
+        for i in range(matrix.shape[0]):
+            for j in range(i + 1, matrix.shape[1]):
+                if matrix[i, j] != matrix[j, i]:
+                    raise ValueError("Matrix must be symmetric")
+
+        self._A = matrix
+
+    def jacobi_rotation(self):
+
+        A = self.A.copy()
+        U_s = []
+        n = len(A)
+
+        while True:
+
+            # Find the maximum off-diagonal element
+            max_off_diag = float('-inf')
+            p, q = 0, 0
+            for i in range(n):
+                for j in range(i + 1, n):
+                    if abs(A[i, j]) > max_off_diag:
+                        max_off_diag = abs(A[i, j])
+                        p, q = i, j
+
+            # Compute the rotation angle
+            theta = 0.5 * np.arctan(2 * A[p, q] / (A[p, p] - A[q, q])) if A[p, p] != A[q, q] else np.pi / 4
+
+            # Compute the rotation matrix
+            c = np.cos(theta)
+            s = np.sin(theta)
+            U = np.eye(n)
+            U[p, p] = c
+            U[q, q] = c
+            U[p, q] = -s
+            U[q, p] = s
+
+            # Update A and U
+            A = U.T @ A @ U
+            U_s.append(U)
+
+            # Check the converegence
+            conv = 0
+            for i in range(n):
+                for j in range(i + 1, n):
+                    conv += A[i, j] ** 2
+            conv = np.sqrt(conv)
+            if conv < self.eps:
+                return np.diag(A), np.linalg.multi_dot(U_s)
+
+
+A = np.array([[9, -2, 3],
+              [-2, 6, 8],
+              [3, 8, -6]])
+
+solver = Eigen(A, 1e-100)
+eigenvalues, eigenvectors = solver.jacobi_rotation()
+print("Eigenvalues:", eigenvalues)
+print("Eigenvectors:\n", eigenvectors)
--- NumericalMethods/README.md
+++ NumericalMethods/README.md
@@ -4,3 +4,4 @@
 * Part 1:  Solving linear equations systems with LU decomposition
 * Part 2: Solving 3-Diagonal linear equations systems
 * Part 3: Solving linear equations systems with Jacobi and Gauss-Seidel iteraion methods
+* Part 4: Eigenvector and eigenvalue calculation with rotation method
